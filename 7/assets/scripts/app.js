955742d3c663eea96eecc5a2dd1dd02343d51d06
Dmitriy <demon-x.88@mail.ru>
2020-12-05 12:47:10 +0300

    Update app.js
    добавил привязку ID

--- 7/assets/scripts/app.js
+++ 7/assets/scripts/app.js
@@ -7,6 +7,8 @@ const addModalActionBtn = cancelBtn.querySelector ('.btn--success');
 const delWin = document.getElementById ('delete-modal');
 const ul = document.querySelector ('#movie-list');
 const inputRating = document.querySelector ('#rating');
+let listMovieId = [];
+
 
 addBtn.onclick =  () => {
     modalWin.classList.add('visible');
@@ -24,6 +26,8 @@ backDrp.onclick = () => {
     delWin.classList.remove('visible');
 }
 
+
+
 addModalActionBtn.onclick = () => {
     if (inputRating.value < 1 || inputRating > 5) {
         alert('Введите рейтинг от 1 до 5');
@@ -31,8 +35,11 @@ addModalActionBtn.onclick = () => {
         let sectionEntry = document.querySelector ('body > main > section');
         if (sectionEntry) {
             sectionEntry.remove();
-        }   
-            
+        }  
+        
+        
+        listMovieId.push(Math.random());
+        
         ul.insertAdjacentHTML("afterbegin","<li class=movie-element><div class=movie-element__image><img src=" 
         + document.getElementById ('image-url').value + " alt=></div><div class=movie-element__info><h2>"
         + document.getElementById ('title').value + "</h2><p>"
@@ -40,24 +47,37 @@ addModalActionBtn.onclick = () => {
         modalWin.classList.remove('visible');
         backDrp.classList.remove ('visible');
    }
-   const removeModalMovie = document.querySelector ('.movie-element');
-   removeModalMovie.addEventListener('click', function (event) {
-        delWin.classList.add ('visible');
-        backDrp.classList.add ('visible'); 
+   const removeModalMovie = document.querySelectorAll('li.movie-element');
+   for (let i=0; i < removeModalMovie.length; i++) {
+        removeModalMovie[i].id = listMovieId[i];
+        removeModalMovie[i].onclick = () => {
+            console.log(removeModalMovie[i]);
+            console.log(listMovieId[i]);
+            delWin.classList.add ('visible');
+            backDrp.classList.add ('visible'); 
+
+            let yesBtn = document.querySelector ('#delete-modal').lastElementChild.lastElementChild;  
         
-        yesBtn.addEventListener('click', function (){
-            event.target.closest('li').remove();      
-            delWin.classList.remove ('visible');
-            backDrp.classList.remove ('visible'); 
-        })
-                  
-    })
-   
-    let noBtn = document.querySelector ('#delete-modal').lastElementChild.firstElementChild;
-    let yesBtn = document.querySelector ('#delete-modal').lastElementChild.lastElementChild;
+            yesBtn.onclick = () => {
+                elem = document.getElementById (listMovieId[i]); 
+                console.log(elem); 
+                elem.parentNode.removeChild(elem);  
+                delWin.classList.remove ('visible');
+                backDrp.classList.remove ('visible'); 
+            }
+
+            let noBtn = document.querySelector ('#delete-modal').lastElementChild.firstElementChild;
             
-    noBtn.onclick = () => {
-        backDrp.classList.remove ('visible');
-        delWin.classList.remove('visible');
-    }
+            noBtn.onclick = () => {
+                backDrp.classList.remove ('visible');
+                delWin.classList.remove('visible');
+            }
+                      
+        }
+   }
+   
+   
+   
+   
+    
 }
