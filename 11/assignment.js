96e60eed1b79f80bf062ebc3b747deaae712aca7
Dmitriy <demon-x.88@mail.ru>
2021-01-24 12:40:21 +0300

    ex num 11.2

--- 11/assignment.js
+++ 11/assignment.js
@@ -4,11 +4,22 @@ class Course {
         this.length = length
         this.price = price
     }
-
+    Price1Min() {
+        let price = this.price;
+        let length = this.length;
+        let end = price/length;
+        return console.log(end + "$ " + " стоимость за одну минуту обучения");
+    }
+    aboutOfCourse () {
+        console.log ("Курс:" + this.title + ": " + "продолжительностью " + this.length + "цена - " + this.price);
+    }
 }
 
-let practice = new Course ("Практическое занятие", "3 часа", "цена 1 млн");
+let practice = new Course ("Практическое занятие", 30, 1000);
 console.log (practice);
+practice.Price1Min();
+practice.aboutOfCourse();
 
-let Theory = new Course ("JavaScript", "5 часов", "1 bil$");
+let Theory = new Course ("JavaScript", 50, 1000);
 console.log(Theory);
+Theory.Price1Min();
