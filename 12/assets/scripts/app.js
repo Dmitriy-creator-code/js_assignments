93f17516387a272965749c90045edc322fc7164e
Dmitriy <demon-x.88@mail.ru>
2021-01-31 11:37:47 +0300

    ToDoList 12.2

--- 12/assets/scripts/app.js
+++ 12/assets/scripts/app.js
@@ -2,14 +2,32 @@ class App {
     
     constructor (selector1) {
         this.section = document.querySelectorAll (selector1);
+
     }
     
-    
+    moreInfo () {
+        this.section.forEach(function(section){
+            const ul = section.childNodes[3];
+            const li = ul.querySelectorAll ('li');
+            li.forEach (function(li){
+                const info = li.querySelectorAll('.alt');
+                info.forEach(function(btn){
+                    btn.addEventListener('click', function(){
+                        let block = document.getElementsByTagName ('body');
+                        let div = document.createElement ('div');
+                        div.innerHTML = "<h4>Hello World!!!</h4>";
+                        div.setAttribute('class', 'card');
+                        document.body.append(div);
+                    })
+                })
+                
+            })
+        })
+    }
     activation () {
         this.section.forEach(function(section){
             const li = section.querySelectorAll ('li');
             li.forEach(function(elem) {
-                console.log(elem);
                 const btn = elem.querySelectorAll('button')[1]; 
                 btn.addEventListener ('click', function(){
                     let ul = elem.parentNode.parentNode;
@@ -29,6 +47,6 @@ class App {
 }
 
 let aboutThisApp = new App("section");
+aboutThisApp.moreInfo();
 aboutThisApp.activation();
-
 
